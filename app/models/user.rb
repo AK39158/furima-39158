d0fcf55df32a19b0bca38f1b8613e18c3530892c
AK39158 <kei199701@gmail.com>
2023-08-02 18:44:31 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-         has_many :items
-         has_many :orders
+  has_many :items
+  has_many :orders
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birth_date
 end
