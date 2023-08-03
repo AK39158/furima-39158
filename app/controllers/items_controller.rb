b35c36bb4177c4b0483baa8ce17b4c6f8be65146
AK39158 <kei199701@gmail.com>
2023-08-03 23:10:32 +0900

    修正とコントロール追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -8,4 +9,40 @@ class ItemsController < ApplicationController
   def new
     @items = Item.new
   end
+
+  def create
+    @items = Item.new(prototype_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  end
+
+  def update
+    if @items.update(item_params)
+      redirect_to item_path(@items)
+    else
+      render :edit
+    end
+  end
+
+  end
+
+
+
+  def show
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price, :category_id, :item_condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @items = Item.find(params[:id])
+  end
+
+
 end
