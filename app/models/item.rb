2835aa38ad5d3af8c129386b8b824d55d48accc5
AK39158 <kei199701@gmail.com>
2023-08-03 20:34:51 +0900

    item　アソシエーションとバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+   # アソシエーション
+   belongs_to :user
+   has_one    :order
+
+  with_options presence: true do
+    validates :user
+    validates :image
+    validates :name 
+    validates :introduction
+    validates :p:redirect
+    validates :category_:id 
+    validates :item_condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+
 end
