9c022b01e72811363b072b587d6208650dc1e50c
AK39158 <kei199701@gmail.com>
2023-08-05 09:18:29 +0900

    修正とコントローラー作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: :sign_up, keys: [:nickname, :first_name, :first_name, :first_name_kana, :first_name_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_up, keys: :sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_date])
   end
 end
