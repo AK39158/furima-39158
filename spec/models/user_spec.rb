4f62f79607bb058e7d64f474ba345e604ef19593
AK39158 <kei199701@gmail.com>
2023-08-08 15:55:29 +0900

    エラーメッセージとfactory-bot搭載

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
-RSpec.describe User, type: :model do
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
