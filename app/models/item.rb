4b37956bbb6a00209ce6ab230747669294ea876e
AK39158 <kei199701@gmail.com>
2023-08-11 12:26:35 +0900

    アクティブハッシュ搭載
    モデルの再作成
    カテゴリーまで作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,18 +1,19 @@
-# class Item < ApplicationRecord
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to :user
+   has_one    :order
 
-#    belongs_to :user
-#    has_one    :order
-
-#   with_options presence: true do
-#     validates :user
-#     validates :image
-#     validates :name
-#     validates :introduction
-#     validates :price
-#     validates :category_id
-#     validates :item_condition_id
-#     validates :shipping_cost_id
-#     validates :prefecture_id
-#     validates :shipping_date_id
-#   end
-# end
+  with_options presence: true do
+    validates :user
+    validates :image
+    validates :name
+    validates :introduction
+    validates :price
+    validates :category_id
+    validates :item_condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+end
