c5963af09bc3b28c440d2b477e7ec4aa9c131c4e
AK39158 <kei199701@gmail.com>
2023-08-16 10:01:40 +0900

    バリデーツ

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,4 +18,12 @@ class Item < ApplicationRecord
   end
 
   has_one_attached :image
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :item_condition_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+  end
 end
