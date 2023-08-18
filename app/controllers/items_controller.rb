c7ee0d9a03f43789b9c7c4ce472840909ff43aa2
AK39158 <kei199701@gmail.com>
2023-08-18 16:25:00 +0900

    indexの搭載とsold out表記
    sold outはorderとリンクさせているためモデルとして作成してます

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   # before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  # def index
-  #   @item = Item.includes(:user)
-  # end
+  def index
+    @items = Item.includes(:user)
+  end
 
   def new
     @item = Item.new
@@ -39,14 +39,14 @@ class ItemsController < ApplicationController
   # def show
   # end
 
-  # def destroy
-  #   if @item.user_id == current_user.id
-  #     @item.destroy
-  #     redirect_to root_path
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   private
 
