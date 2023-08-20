a9c64e6a9b30fa6e054a8a1cad2ac39edf1100dc
AK39158 <kei199701@gmail.com>
2023-08-20 16:34:03 +0900

    表示変更

--- db/migrate/20230811015959_create_items.rb
+++ db/migrate/20230811015959_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.references :user,           null: false, foreign_key: true
       t.string :name,               null: false
-      t.integer :introduction,      null: false
+      t.text :introduction,      null: false
       t.integer :price,             null: false
       t.integer :category_id,       null: false
       t.integer :item_condition_id, null: false
