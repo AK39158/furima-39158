25baec52fbafb0611944b473cb90de9e523d6449
AK39158 <kei199701@gmail.com>
2023-08-23 21:41:49 +0900

    モデル編集

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -14,5 +14,7 @@ class OrderForm
   end
   
   def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Addresser.create(order_id: order.id, post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
   end
 end
