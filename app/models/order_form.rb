4099945e3a0203e45a2a6e115bd27005beea9677
AK39158 <kei199701@gmail.com>
2023-08-23 21:21:02 +0900

    コントローラー編集

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,15 +1,18 @@
-class クラス名
+class OrderForm
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :house_number, :buildingzz_name, :phone_number
+  attr_accessor :user_id, :item_id,:city, :post_code, :prefecture_id, :house_number, :building_name, :phone_number, :token
 
-  
   with_options presence: true do
-    validates :post_code
-    validates :prefecture_id
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
     validates :house_number
-    validates :buildingzz_name
-    validates :phone_number
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+    validates :token
   end
   
   def save
-    
+  end
+end
